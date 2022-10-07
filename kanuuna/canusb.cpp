4b167939d858ea677a501eb5d98078f94958bf88
Leo Lahti <leo.m.lahti@aalto.fi>
2022-10-07 12:23:09 +0300

    Working preliminarily

---
+++ kanuuna/canusb.cpp
@@ -0,0 +1,158 @@
+#include <mcp2515.h>
+#include <stdlib.h>
+#include "canusb.h"
+#include "util.h"
+
+/* Parse CAN_ID into the structure. Can use both normal and extended formats */
+static unsigned int parse_id(const char *iid, unsigned int id_len);
+
+static unsigned int parse_data(const char *msg, struct can_frame *fr);
+
+static unsigned int parse_id(const char *iid, unsigned int id_len)
+{
+  unsigned int id;
+
+  id = 0;
+  for (int i = 0; i < id_len; i++) {
+    id |= hex_to_nib(iid[i]) << ((id_len - 1)*4 - i*4);
+  }
+
+  return id;
+}
+
+static unsigned int parse_data(const char *msg, struct can_frame *fr)
+{
+
+    fr->can_dlc = hex_to_nib(msg[0]);
+    for (int i = 0; i < fr->can_dlc; i++) {
+      fr->data[i] =  hex_to_nib(msg[i * 2 + 1]) << 4;
+      fr->data[i] |= hex_to_nib(msg[i * 2 + 2]);
+    }
+
+  return 0;
+}
+
+const char * parse_canusb(const char *msg, MCP2515 mcp)
+{
+  struct can_frame fr;
+  // Serial.println(msg[1]);
+  // return msg;
+  switch (msg[0]) {
+    case 'S':
+      switch (msg[1]) {
+        case '0':
+          if(mcp.setBitrate(CAN_10KBPS) != MCP2515::ERROR_OK)
+            return "\a";
+          return "\r";
+          break;
+        case '1':
+          if(mcp.setBitrate(CAN_20KBPS) != MCP2515::ERROR_OK)
+            return "\a";
+          return "\r";
+          break;
+        case '2':
+          if(mcp.setBitrate(CAN_50KBPS) != MCP2515::ERROR_OK)
+            return "\a";
+          return "\r";
+          break;
+        case '3':
+          if(mcp.setBitrate(CAN_100KBPS) != MCP2515::ERROR_OK)
+            return "\a";
+          return "\r";
+          break;
+        case '4':
+          if(mcp.setBitrate(CAN_125KBPS) != MCP2515::ERROR_OK)
+            return "\a";
+          return "\r";
+          break;
+        case '5':
+          if(mcp.setBitrate(CAN_250KBPS) != MCP2515::ERROR_OK)
+            return "\a";
+          return "\r";
+          break;
+        case '6':
+          if(mcp.setBitrate(CAN_500KBPS) != MCP2515::ERROR_OK)
+            return "\a";
+          return "\r";
+          break;
+        case '7':
+          return "\a";
+          break;
+        case '8':
+          if(mcp.setBitrate(CAN_1000KBPS) != MCP2515::ERROR_OK)
+            return "\a";
+          return "\r";
+          break;
+        default:
+          return "\a";
+      }
+      break;
+    case 's':
+      return "\a";
+      break;
+    case 'O':
+      if (mcp.setNormalMode() != MCP2515::ERROR_OK)
+        return "\a";
+      return "\r";
+    case 'D':
+      if (mcp.setLoopbackMode() != MCP2515::ERROR_OK)
+        return "\a";
+      return "\r";
+    case 'C':
+      if (mcp.reset() != MCP2515::ERROR_OK)
+        return "\a";
+      return "\r";
+    case 't':
+      fr.can_id = parse_id(&msg[1], 3);
+      parse_data(&msg[4], &fr);
+      if (mcp.sendMessage(&fr) != MCP2515::ERROR_OK)
+        return "\a";
+      return "z\r";
+      break;
+    case 'T':
+      fr.can_id = parse_id(&msg[1], 8);
+      parse_data(&msg[9], &fr);
+      if (mcp.sendMessage(&fr) != MCP2515::ERROR_OK)
+        return "\a";
+      return "Z\r";
+      break;
+    case 'r':
+      fr.can_id = parse_id(&msg[1], 3);
+      fr.can_id |= CAN_RTR_FLAG;
+      fr.can_dlc = hex_to_nib(msg[4]);
+      if (mcp.sendMessage(&fr) != MCP2515::ERROR_OK)
+        return "\a";
+      return "z\r";
+      break;
+    case 'R':
+      fr.can_id = parse_id(&msg[1], 8);
+      fr.can_id |= CAN_RTR_FLAG;
+      fr.can_dlc = hex_to_nib(msg[9]);
+      if (mcp.sendMessage(&fr) != MCP2515::ERROR_OK)
+        return "\a";
+      return "Z\r";
+      break;
+    case 'F':
+      return "F00\r";
+      break;
+    case 'M':
+      return "\a";
+      break;
+    case 'm':
+      return "\a";
+      break;
+    case 'V':
+      return "V1013\r";
+      break;
+    case 'N':
+      return "\a";
+      break;
+    case 'Z':
+      return "\a";
+      break;
+    default:
+      return "\a";
+  }
+}
+
+// vim:set ft=arduino
---
+++ kanuuna/kanuuna.ino
@@ -0,0 +1,116 @@
+#include <SPI.h>
+#include <mcp2515.h>
+
+#include "canusb.h"
+#include "util.h"
+
+#define BAUD 9600
+#define QLEN 8
+
+struct can_queue {
+  struct can_frame q[QLEN];
+  byte head, tail; /* Next to pop, next to push. Mod QLEN*/
+};
+
+
+byte can_queue_push(struct can_frame msg, struct can_queue *q);
+struct can_frame can_queue_pop(struct can_queue *q);
+
+void int_handler();
+void write_to_serial(const struct can_frame cf);
+bool read_from_serial();
+
+const int baud = BAUD;
+bool interrupt = false;
+
+MCP2515 mcp(10);
+struct can_frame can_msg, cmsg;
+struct can_queue queue;
+
+byte can_queue_push(struct can_frame msg, struct can_queue *q) {
+  q->q[q->tail] = msg;
+  q->tail = (q->tail + 1) % QLEN;
+  return q->tail;
+}
+
+struct can_frame can_queue_pop(struct can_queue *q) {
+  struct can_frame ret;
+
+  ret = q->q[q->head];
+  q->head = (q->head + 1) % QLEN;
+  return ret;
+}
+
+void int_handler() {
+  interrupt = true;
+}
+void write_to_serial(const struct can_frame cf) {
+  /* char buf[19];
+  int i;
+  memset(&buf[0], 0, sizeof(buf));
+  for (i = 7; i <= 0; i--) {
+    buf[i] = byte_to_hex(cf.can_id >> i*4);
+  }
+  buf[8] = byte_to_hex(cf.can_dlc);
+  for (i = 0; i < cf.can_dlc; i++) {
+    buf[9 + i*2] = byte_to_hex(cf.data[i] >> 4);
+    buf[9 + i*2 + 1] = byte_to_hex(cf.data[i]);
+  }
+  buf[9 + i*2 + 2] = '\r';
+  Serial.print(buf); */
+  Serial.print("t");
+  Serial.print(can_msg.can_id, HEX);
+  Serial.print(can_msg.can_dlc, HEX);
+  for (int i = 0; i < can_msg.can_dlc; i++) {
+    Serial.print(can_msg.data[i], HEX);
+  }
+  Serial.write('\r');
+}
+
+bool read_from_serial() {
+
+  char msg[19];
+  String s;
+
+  memset(&msg[0], 0, sizeof(msg));
+  s = Serial.readStringUntil('\r');
+  /* Serial.print(s); */
+
+  s.toCharArray(msg, 19);
+  Serial.print(parse_canusb(msg, mcp));
+
+  return true;
+}
+
+
+void setup() {
+  while(!Serial);
+  Serial.begin(baud);
+  SPI.begin();
+  /* mcp.reset(); */
+  /* pinMode(2, INPUT_PULLUP); */
+  /* attachInterrupt(0, int_handler, FALLING); */
+  queue.head = 0;
+  queue.tail = 0;
+}
+
+void loop() {
+  MCP2515::ERROR err;
+  /* if (queue.head != queue.tail) {
+    cmsg = can_queue_pop(&queue);
+    write_to_serial(cmsg);
+  } */
+  if (Serial.available() > 0) {
+    if (!read_from_serial()) {
+      Serial.println("Error");
+      exit(1);
+    }
+  }
+  err = mcp.readMessage(&can_msg);
+  if (err == MCP2515::ERROR_OK) {
+    write_to_serial(cmsg);
+    /* can_queue_push(can_msg, &queue); */
+  } else if (err != MCP2515::ERROR_NOMSG) {
+    Serial.print("\a");
+  }
+}
